8b54c1ad4c960fca66aee137c757de096d83847e
Madeleine <mgleahy@nic.edu>
2023-11-07 15:29:01 -0800

    made the list disappear
    added some javascript.  It needs to be adjusted.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// create a var
+const hamburger = document.querySelector('.hamburger');
 // the window needs to be a certain size
 
 
 // the hamburger is clicked then
+hamburger.addEventListener('click',function (){
+    hamburger.classList.toggle('active');
+})
     // the list needs to pop down
+    if (x === list.active ) {
+
+    }
 // when the hamburger is clicked again the list needs to close
